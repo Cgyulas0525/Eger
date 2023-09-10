de3d62d3d1464b19c8e25d0aa71a43b7f1a7e4c1
Cgyulas0525 <cseszneki.gyula@gmail.com>
2023-09-10 16:52:47 +0200

    D-7 SettlementsSeeder

--- database/seeders/SettlementsSeeder.php
+++ database/seeders/SettlementsSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use App\Models\Settlements;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use File;
+use Illuminate\Support\Facades\Storage;
 
 class SettlementsSeeder extends Seeder
 {
@@ -14,11 +14,11 @@ class SettlementsSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
         Settlements::truncate();
 
-        $json = File::get(storage_path() . '/data/settlements.data');
+        $json = Storage::disk('data')->get('settlements.data');;
         $settlements = json_decode($json, true);
 
         foreach ($settlements as $key => $settlement) {
