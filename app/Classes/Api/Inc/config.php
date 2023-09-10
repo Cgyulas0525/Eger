a3371b2f09d41cf9e75529e8af7a5db29839aa93
Cgyulas0525 <cseszneki.gyula@gmail.com>
2023-09-10 15:28:17 +0200

    D-7 ClientsFactory.php ClientquestionnariesFactory.php ClientquestionnariedetailsFactory.php

--- app/Classes/Api/Inc/config.php
+++ app/Classes/Api/Inc/config.php
@@ -3,7 +3,7 @@ ini_set('memory_limit', '-1');
 ini_set('max_execution_time', 6000);
 
 date_default_timezone_set("Europe/Budapest");
-
+0
 define("PROJECT_ROOT_PATH", dirname(__DIR__, 3));
 define("PATH_INC", dirname(__DIR__, 2). '/Classes/Api/Inc');
 define('PATH_OUTPUT', dirname(__DIR__, 4) . '/storage/output/');
